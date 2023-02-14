1548b29fdafaa742fb920abcfb8b66e32274a643
FilipRazek <65445960+FilipRazek@users.noreply.github.com>
2023-02-14 18:33:29 +0100

    Add products to database

--- src/products-data.js
+++ src/products-data.js
@@ -3,9 +3,14 @@ export const BILLA_PRODUCTS = {
   82246186:
     "lindt-excellence-horka-cokolada-s-pomerancovymi-kousky-a-s-kousky-mandli-100g-82246186",
   82329725: "pcukr-krupice-1kg-korunnitp-82329725",
+  82334636: "billa-premium-cerstva-vejce-slepic-ve-volnem-vybehu-ml-6-ks-82334636",
+  82336414: "cuketa-82336414",
 };
 
 export const PRODUCTS = {
-  TESCO: [2001012691222, 2001014250175, 2001014531595],
-  BILLA: [82329695, 82246186, 82329725],
+  TESCO: [
+    2001012691222, 2001014250175, 2001014531595, 2001012691659, 2001012692878,
+    2001012177306, 2001013321111,
+  ],
+  BILLA: [82329695, 82246186, 82329725, 82334636, 82336414],
 };
